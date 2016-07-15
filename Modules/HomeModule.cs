37c5dde932be6a487b896075974ebd01e67433b0
Joel Waage <joeltwaage@gmail.com>
2016-07-15 13:47:59 -0700

    add links between pages, display contacts in contact list

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,29 @@
+using Nancy;
+using ContactSheet.Objects;
+using System.Collections.Generic;
+
+namespace ContactSheet
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => {
+        return View["index.cshtml"];
+      };
+      Get["/new-contact"] = _ => {
+        return View["new_contact.cshtml"];
+      };
+      Get["/contact-list"] = _ => {
+        List<Contact> allContacts = Contact.GetAll();
+        return View["contact_list.cshtml", allContacts];
+      };
+      Post["/name-added"] = _ => {
+        Contact newContact = new Contact(Request.Form["contact-name"], Request.Form["contact-phone"], Request.Form["contact-address"]);
+        newContact.Save();
+        return View["contact_created.cshtml", newContact];
+      };
+
+    }
+  }
+}
---
+++ Objects/Contact.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+
+namespace ContactSheet.Objects
+{
+  public class Contact
+  {
+//set strings, etc.
+    private string _contactName;
+    private string _contactPhone;
+    private string _contactAddress;
+    private static List<Contact> _instances = new List<Contact> {};
+//constructor
+    public Contact (string contactName, string contactPhone, string contactAddress)
+    {
+      _contactName = contactName;
+      _contactPhone = contactPhone;
+      _contactAddress = contactAddress;
+    }
+//getters and setters
+    public void SetName(string newName)
+      {
+        _contactName = newName;
+      }
+    public string GetName()
+    {
+      return _contactName;
+    }
+
+    public void SetPhone(string newPhone)
+    {
+      _contactPhone = newPhone;
+    }
+    public string GetPhone()
+    {
+      return _contactPhone;
+    }
+
+    public void SetAddress(string newAddress)
+    {
+      _contactAddress = newAddress;
+    }
+    public string GetAddress()
+    {
+      return _contactAddress;
+    }
+
+    public static List<Contact> GetAll()
+    {
+      return _instances;
+    }
+    public void Save()
+    {
+      _instances.Add(this);
+    }
+
+  }
+}
